a5c730b01f1e3cbe094dc83dd675418604d2a55f
DubbedDetective <155699317+DubbedDetective@users.noreply.github.com>
2025-04-16 16:41:51 -0500

    Add file dialogue selection to allow selecting multiple PDF files

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,21 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.InformationToFill_dataGridView = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.InformationToFill_dataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // InformationToFill_dataGridView
+            // 
+            this.InformationToFill_dataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.InformationToFill_dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.InformationToFill_dataGridView.Location = new System.Drawing.Point(23, 62);
+            this.InformationToFill_dataGridView.Name = "InformationToFill_dataGridView";
+            this.InformationToFill_dataGridView.RowHeadersVisible = false;
+            this.InformationToFill_dataGridView.Size = new System.Drawing.Size(752, 361);
+            this.InformationToFill_dataGridView.TabIndex = 0;
+            this.InformationToFill_dataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.InformationToFill_dataGridView_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Select the Document(s) to Fill";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(178, 35);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(39, 20);
+            this.numericUpDown1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(175, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "How many rows to generate";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 30);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Select PDFs";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.InformationToFill_dataGridView);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Mass Fill PDF";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.InformationToFill_dataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView InformationToFill_dataGridView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using iText.Forms;
+using iText.Forms.Fields;
 
 namespace Mass_Fill_PDF
 {
@@ -20,6 +22,37 @@ namespace Mass_Fill_PDF
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void InformationToFill_dataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void DocumentsToFill_comboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = true;
+            dialog.Filter = "PDF files(*.pdf)|*.pdf";
+            if(dialog.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show("Success");
+            }
         }
     }
 }
