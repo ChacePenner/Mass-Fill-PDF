fc05f8472b79614b056cd750fe4c8289d71ce3c0
Chace Penner <penner.chace.github@gmail.com>
2025-04-24 16:24:18 -0500

    Merge working with master (#1)
    * Add iText package
    
    * Add file dialogue selection to allow selecting multiple PDF files
    
    * Add functionality to pull PDF form name and populate Data Grid View columns with form field names.
    
    * Add error handling to check for PDFs without form fields. Add clear button with warning prompt.
    
    * Add helper method to retrieve form field type.
    
    * Add support for textfield, textarea, radio, checkbox, and dropdown.
    
    * Add functionality to preload data.
    
    * Add support to initialized checkboxes
    
    * Add support to print PDF for each row. Add dropdown to allow user to select the column to use in naming the document.
    
    * Limit naming box to string fields only.
    
    * Add string trimming.
    
    * Implement support for writing to multiple PDFs at once.
    
    * Add error handling to prevent loading a bad PDF.
    
    * Add tooltip text to guide user.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,12 +29,195 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.InformationToFill_dataGridView = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rowsToGenerate_numericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.selectPDF_button = new System.Windows.Forms.Button();
+            this.generateRows_button = new System.Windows.Forms.Button();
+            this.clearTable_button = new System.Windows.Forms.Button();
+            this.fillPDF_button = new System.Windows.Forms.Button();
+            this.addRow_button = new System.Windows.Forms.Button();
+            this.namingComboBox = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.selectPDF_tooltip = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.InformationToFill_dataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rowsToGenerate_numericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // InformationToFill_dataGridView
+            // 
+            this.InformationToFill_dataGridView.AllowUserToAddRows = false;
+            this.InformationToFill_dataGridView.AllowUserToDeleteRows = false;
+            this.InformationToFill_dataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.InformationToFill_dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.InformationToFill_dataGridView.Location = new System.Drawing.Point(23, 62);
+            this.InformationToFill_dataGridView.Name = "InformationToFill_dataGridView";
+            this.InformationToFill_dataGridView.RowHeadersVisible = false;
+            this.InformationToFill_dataGridView.Size = new System.Drawing.Size(752, 383);
+            this.InformationToFill_dataGridView.TabIndex = 0;
+            this.InformationToFill_dataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.InformationToFill_dataGridView_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "1. Select the PDF(s) to Fill";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // rowsToGenerate_numericUpDown
+            // 
+            this.rowsToGenerate_numericUpDown.Location = new System.Drawing.Point(165, 35);
+            this.rowsToGenerate_numericUpDown.Name = "rowsToGenerate_numericUpDown";
+            this.rowsToGenerate_numericUpDown.Size = new System.Drawing.Size(37, 20);
+            this.rowsToGenerate_numericUpDown.TabIndex = 4;
+            this.selectPDF_tooltip.SetToolTip(this.rowsToGenerate_numericUpDown, "Select the number of rows to generate.");
+            this.rowsToGenerate_numericUpDown.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.rowsToGenerate_numericUpDown.ValueChanged += new System.EventHandler(this.rowsToGenerate_numericUpDown_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(162, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(151, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "2. How many rows to generate";
+            // 
+            // selectPDF_button
+            // 
+            this.selectPDF_button.Location = new System.Drawing.Point(23, 28);
+            this.selectPDF_button.Name = "selectPDF_button";
+            this.selectPDF_button.Size = new System.Drawing.Size(88, 30);
+            this.selectPDF_button.TabIndex = 7;
+            this.selectPDF_button.Text = "Select PDFs";
+            this.selectPDF_tooltip.SetToolTip(this.selectPDF_button, "Select one or more PDFs with form fields.");
+            this.selectPDF_button.UseVisualStyleBackColor = true;
+            this.selectPDF_button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // generateRows_button
+            // 
+            this.generateRows_button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.generateRows_button.Location = new System.Drawing.Point(625, 12);
+            this.generateRows_button.Name = "generateRows_button";
+            this.generateRows_button.Size = new System.Drawing.Size(150, 43);
+            this.generateRows_button.TabIndex = 8;
+            this.generateRows_button.Text = "3. Generate Rows for Selected PDF(s)";
+            this.selectPDF_tooltip.SetToolTip(this.generateRows_button, "Generate the specified number of rows for the PDF(s) selected.");
+            this.generateRows_button.UseVisualStyleBackColor = true;
+            this.generateRows_button.Click += new System.EventHandler(this.generateRows_button_Click);
+            // 
+            // clearTable_button
+            // 
+            this.clearTable_button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.clearTable_button.Location = new System.Drawing.Point(619, 473);
+            this.clearTable_button.Name = "clearTable_button";
+            this.clearTable_button.Size = new System.Drawing.Size(75, 23);
+            this.clearTable_button.TabIndex = 9;
+            this.clearTable_button.Text = "Clear Table";
+            this.selectPDF_tooltip.SetToolTip(this.clearTable_button, "Clear the data grid of all data. This cannot be undone.");
+            this.clearTable_button.UseVisualStyleBackColor = true;
+            this.clearTable_button.Click += new System.EventHandler(this.clearTable_button_Click);
+            // 
+            // fillPDF_button
+            // 
+            this.fillPDF_button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.fillPDF_button.Location = new System.Drawing.Point(700, 473);
+            this.fillPDF_button.Name = "fillPDF_button";
+            this.fillPDF_button.Size = new System.Drawing.Size(75, 23);
+            this.fillPDF_button.TabIndex = 10;
+            this.fillPDF_button.Text = "5. Fill PDF(s)";
+            this.selectPDF_tooltip.SetToolTip(this.fillPDF_button, "Generate and fill in PDFs with the provided data to a chosen location.");
+            this.fillPDF_button.UseVisualStyleBackColor = true;
+            this.fillPDF_button.Click += new System.EventHandler(this.fillPDF_button_Click);
+            // 
+            // addRow_button
+            // 
+            this.addRow_button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.addRow_button.Location = new System.Drawing.Point(23, 473);
+            this.addRow_button.Name = "addRow_button";
+            this.addRow_button.Size = new System.Drawing.Size(75, 23);
+            this.addRow_button.TabIndex = 11;
+            this.addRow_button.Text = "Add Row";
+            this.selectPDF_tooltip.SetToolTip(this.addRow_button, "Add one row to the data grid.");
+            this.addRow_button.UseVisualStyleBackColor = true;
+            this.addRow_button.Click += new System.EventHandler(this.addRow_button_Click);
+            // 
+            // namingComboBox
+            // 
+            this.namingComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.namingComboBox.FormattingEnabled = true;
+            this.namingComboBox.Location = new System.Drawing.Point(355, 475);
+            this.namingComboBox.Name = "namingComboBox";
+            this.namingComboBox.Size = new System.Drawing.Size(258, 21);
+            this.namingComboBox.TabIndex = 12;
+            this.selectPDF_tooltip.SetToolTip(this.namingComboBox, "Choose a column in which each row has a UNIQUE value to append to the end of the " +
+        "saved file.");
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(352, 459);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(262, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "4. Select header to use to generate unique file names:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // selectPDF_tooltip
+            // 
+            this.selectPDF_tooltip.Popup += new System.Windows.Forms.PopupEventHandler(this.selectPDF_tooltip_Popup);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(800, 505);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.namingComboBox);
+            this.Controls.Add(this.addRow_button);
+            this.Controls.Add(this.fillPDF_button);
+            this.Controls.Add(this.clearTable_button);
+            this.Controls.Add(this.generateRows_button);
+            this.Controls.Add(this.selectPDF_button);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.rowsToGenerate_numericUpDown);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.InformationToFill_dataGridView);
+            this.Name = "Form1";
+            this.Text = "Mass Fill PDF";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.InformationToFill_dataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rowsToGenerate_numericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView InformationToFill_dataGridView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown rowsToGenerate_numericUpDown;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button selectPDF_button;
+        private System.Windows.Forms.Button generateRows_button;
+        private System.Windows.Forms.Button clearTable_button;
+        private System.Windows.Forms.Button fillPDF_button;
+        private System.Windows.Forms.Button addRow_button;
+        private System.Windows.Forms.ComboBox namingComboBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ToolTip selectPDF_tooltip;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,20 +1,409 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using iText.Forms;
+using iText.Forms.Fields;
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Annot;
+using iText.Kernel.Exceptions;
 
 namespace Mass_Fill_PDF
 {
     public partial class Form1: Form
     {
+        private string[] selectedFiles;
+        private int rowsToGenerate = 1;
+        private Dictionary<string, string> pdfDefaultFieldValues = new Dictionary<string, string>();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InformationToFill_dataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void DocumentsToFill_comboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = true;
+            dialog.Filter = "PDF files(*.pdf)|*.pdf";
+
+            if(dialog.ShowDialog() == DialogResult.OK)
+            {
+                selectedFiles = dialog.FileNames;
+                MessageBox.Show("PDF(s) selected successfully.");
+            }
+            else
+            {
+                selectedFiles = null;
+            }
+        }
+
+        private void generateRows_button_Click(object sender, EventArgs e)
+        {
+            if (selectedFiles == null || selectedFiles.Length == 0)
+            {
+                MessageBox.Show("No PDF selected. Please select one or more first.");
+                return;
+            }
+            if (rowsToGenerate <= 0)
+            {
+                MessageBox.Show("Rows to generate must be greater than 0.");
+                return;
+            }
+
+            var allFieldtypes = new Dictionary<string, PdfName>();
+            var mergedDefaults = new Dictionary<string, string>();
+
+            foreach (string path in selectedFiles)
+            {
+                var types = GetFormFieldTypes(path);
+                if (types == null)
+                {
+                    return;
+                }
+                var defaults = GetDefaultFieldValues(path);
+                if (defaults == null)
+                {
+                    return;
+                }
+
+                foreach (var kv in types)
+                {
+                    if (!allFieldtypes.ContainsKey(kv.Key))
+                    {
+                        allFieldtypes[kv.Key] = kv.Value;
+                    }
+                }
+
+                foreach (var kv in defaults)
+                {
+                    if (!mergedDefaults.ContainsKey(kv.Key) || string.IsNullOrWhiteSpace(mergedDefaults[kv.Key]))
+                    {
+                        mergedDefaults[kv.Key] = kv.Value;
+                    }
+                }
+            }
+
+            InformationToFill_dataGridView.Columns.Clear();
+
+            foreach (var kv in allFieldtypes)
+            {
+                string name = kv.Key;
+                PdfName type = kv.Value;
+
+                if (PdfName.Btn.Equals(type))
+                {
+                    InformationToFill_dataGridView.Columns.Add(new DataGridViewCheckBoxColumn { Name = name, HeaderText = name });
+                }
+
+                else if (PdfName.Ch.Equals(type))
+                {
+                    var combo = new DataGridViewComboBoxColumn { Name = name, HeaderText = name };
+                    combo.Items.AddRange(PdfAcroForm.GetAcroForm(new PdfDocument(new PdfReader(selectedFiles[0])), false).GetField(name).GetAppearanceStates());
+                    InformationToFill_dataGridView.Columns.Add(combo);
+                }
+                else
+                {
+                    InformationToFill_dataGridView.Columns.Add(name, name);
+                }
+            }
+
+            pdfDefaultFieldValues = mergedDefaults;
+            InformationToFill_dataGridView.Rows.Clear();
+
+            for (int i = 0; i < rowsToGenerate; i++)
+            {
+                if (InformationToFill_dataGridView.Columns.Count <= 0)
+                {
+                    MessageBox.Show("Please select a PDF with form fields.");
+                    return;
+                }
+                int index = InformationToFill_dataGridView.Rows.Add();
+                foreach (DataGridViewColumn column in InformationToFill_dataGridView.Columns)
+                {
+                    if (pdfDefaultFieldValues.TryGetValue(column.Name, out string defVal))
+                    {
+                        var cell = InformationToFill_dataGridView.Rows[index].Cells[column.Index];
+                        if (column is DataGridViewCheckBoxColumn)
+                        {
+                            cell.Value = ParseCheckboxValue(defVal);
+                        }
+                        else
+                        {
+                            cell.Value = defVal;
+                        }
+                    }
+                }
+            }
+
+            namingComboBox.Items.Clear();
+            namingComboBox.Items.Add("Default");
+            foreach (DataGridViewColumn column in InformationToFill_dataGridView.Columns)
+            {
+                if (column is DataGridViewTextBoxColumn || column is DataGridViewComboBoxColumn)
+                {
+                    namingComboBox.Items.Add(column.Name);
+                }
+                namingComboBox.SelectedIndex = 0;
+            }
+        }
+
+        private void rowsToGenerate_numericUpDown_ValueChanged(object sender, EventArgs e)
+        {
+            rowsToGenerate = Convert.ToInt32(rowsToGenerate_numericUpDown.Value);
+        }
+
+        private void addRow_button_Click(object sender, EventArgs e)
+        {
+            if (InformationToFill_dataGridView.Rows.Count == 0)
+            {
+                MessageBox.Show("Additional rows may only be added after first initializing a PDF.");
+                return;
+            }
+
+            int newRowIndex = InformationToFill_dataGridView.Rows.Add();
+            foreach (DataGridViewColumn column in InformationToFill_dataGridView.Columns)
+            {
+                if (pdfDefaultFieldValues.TryGetValue(column.Name, out string defaultValue))
+                { 
+                    if (column is DataGridViewCheckBoxColumn)
+                    {
+                        InformationToFill_dataGridView.Rows[newRowIndex].Cells[column.Index].Value = ParseCheckboxValue(defaultValue);
+                    }
+                    else
+                    {
+                        InformationToFill_dataGridView.Rows[newRowIndex].Cells[column.Index].Value = defaultValue; 
+                    }
+                        
+                }
+            }
+        }
+        private void clearTable_button_Click(object sender, EventArgs e)
+        {
+            DialogResult clearDialog = MessageBox.Show("Are you sure you want to clear all data in the grid? This cannot be undone.", "Clear All Data", MessageBoxButtons.YesNo);
+            if (clearDialog == DialogResult.Yes)
+            {
+                InformationToFill_dataGridView.Columns.Clear();
+                InformationToFill_dataGridView.Rows.Clear();
+            }
+        }
+        private Dictionary<string, PdfName> GetFormFieldTypes(string filePath)
+            //Retrieves the form field types (check box, text, radio button, etc.)
+        {
+            var fieldTypes = new Dictionary<string, PdfName>();
+
+            try
+            {
+                using (var reader = new PdfReader(filePath))
+                using (var pdfDoc = new PdfDocument(reader))
+                {
+                    PdfAcroForm form = PdfAcroForm.GetAcroForm(pdfDoc, false);
+                    if (form == null || form.GetAllFormFields().Count == 0)
+                    {
+                        return fieldTypes;
+                    }
+
+                    foreach (var field in form.GetAllFormFields())
+                    {
+                        PdfName type = field.Value.GetFormType();
+                        fieldTypes.Add(field.Key, type);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error with selected PDF at:\n{Path.GetFileName(filePath)}\n\n{ex.Message}", "Read Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+
+            return fieldTypes;
+        }
+
+        private Dictionary<string, string> GetDefaultFieldValues(string filePath)
+        {
+            var fieldDefaults = new Dictionary<string, string>();
+
+            try
+            {
+                using (var reader = new PdfReader(filePath))
+                using (var pdfDoc = new PdfDocument(reader))
+                {
+                    PdfAcroForm form = PdfAcroForm.GetAcroForm(pdfDoc, false);
+                    if (form == null || form.GetAllFormFields().Count == 0)
+                    {
+                        return fieldDefaults;
+                    }
+
+                    foreach (var field in form.GetAllFormFields())
+                    {
+                        string key = field.Key;
+                        PdfFormField formField = field.Value;
+                        PdfName type = formField.GetFormType();
+
+                        if (PdfName.Btn.Equals(type))
+                        {
+                            PdfDictionary dict = formField.GetPdfObject();
+                            PdfName value = dict.GetAsName(PdfName.V);
+                            fieldDefaults[key] = value != null ? value.ToString() : "Off";
+                        }
+                        else
+                        {
+                            fieldDefaults[key] = formField.GetValueAsString();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to read PDF form fields:\n{ex.Message}", "PDF Load Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return null;
+            }
+            return fieldDefaults;
+        }
+
+        private bool ParseCheckboxValue(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                return false;
+            }
+
+            string normalized = value.TrimStart('/').ToLower();
+            return normalized.StartsWith("yes") || normalized == "1";
+        }
+
+        private void fillPDF_button_Click(object sender, EventArgs e)
+        {
+            //Checks that a template has been selected and rows have been generated
+            if (selectedFiles == null || selectedFiles.Length == 0)
+            {
+                MessageBox.Show("No PDF selected.");
+                return;
+            }
+            if (InformationToFill_dataGridView.Rows.Count == 0)
+            {
+                MessageBox.Show("No data to fill. The grid is empty.");
+                return;
+            }
+
+            using (var folderDialog = new FolderBrowserDialog())
+            {
+                //Allows the user to select where the PDF(s) will be generated.
+                folderDialog.Description = "Select output folder for filled PDFs.";
+                if (folderDialog.ShowDialog() != DialogResult.OK)
+                {
+                    return;
+                }
+                string outFolder = folderDialog.SelectedPath;
+
+                string nameSource = namingComboBox.SelectedItem?.ToString() ?? "Default";
+
+                foreach (string templatePath in selectedFiles)
+                {
+                    string baseName = Path.GetFileNameWithoutExtension(templatePath);
+
+                    //Creates a new PDF for each row of data.
+                    for (int rowIndex = 0; rowIndex < InformationToFill_dataGridView.Rows.Count; rowIndex++)
+                    {
+                        var row = InformationToFill_dataGridView.Rows[rowIndex];
+
+                        string fileName;
+                        if (nameSource != "Default" && row.Cells[nameSource].Value is string raw && !string.IsNullOrWhiteSpace(raw))
+                        {
+                            var invalids = Path.GetInvalidFileNameChars();
+                            var safe = new string(raw.Where(ch => !invalids.Contains(ch)).ToArray()).Trim();
+                            if (string.IsNullOrEmpty(safe))
+                            {
+                                safe = (rowIndex + 1).ToString();
+                            }
+                            fileName = $"{baseName}_{safe}.pdf";
+                        }
+                        else
+                        {
+                            fileName = $"{baseName}_{rowIndex + 1}.pdf";
+                        }
+                        string outputPath = Path.Combine(outFolder, fileName);
+
+                        // Opens the template PDF for reading and writing
+                        using (var reader = new PdfReader(templatePath))
+                        using (var writer = new PdfWriter(outputPath))
+                        using (var pdfDoc = new PdfDocument(reader, writer))
+                        {
+                            var form = PdfAcroForm.GetAcroForm(pdfDoc, true);
+
+                            //Fills in each field from the DataGridView
+
+                            foreach (DataGridViewColumn column in InformationToFill_dataGridView.Columns)
+                            {
+                                string fieldName = column.Name;
+                                var pdfField = form.GetField(fieldName);
+                                if (pdfField == null)
+                                {
+                                    continue;
+                                }
+
+                                var cell = row.Cells[column.Index].Value;
+
+                                if (column is DataGridViewCheckBoxColumn)
+                                {
+                                    bool isChecked = Convert.ToBoolean(cell);
+
+                                    string onValue = form.GetField(fieldName).GetAppearanceStates().FirstOrDefault(s => !s.Equals("Off", StringComparison.OrdinalIgnoreCase)) ?? "Yes";
+                                    form.GetField(fieldName).SetValue(isChecked ? onValue : "Off");
+                                }
+                                else
+                                {
+                                    string value = cell?.ToString().Trim() ?? "";
+                                    form.GetField(fieldName).SetValue(value);
+                                }
+                            }
+                        }
+                    }
+                }
+                MessageBox.Show($"PDFs written to:\n{outFolder}");
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void selectPDF_tooltip_Popup(object sender, PopupEventArgs e)
+        {
         }
     }
 }
